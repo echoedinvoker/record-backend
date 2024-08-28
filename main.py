f387dcd51a06dffb6522f4605089ef1f3cab8643
Matt Chang <echoedinvoker@gmail.com>
2024-08-28 10:22:10 +0800

    add more origins

--- main.py
+++ main.py
@@ -11,7 +11,11 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://app",
+        "http://localhost",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
